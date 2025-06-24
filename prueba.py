c5341ab594cb7f6da973db1b39712d360aa477b4
reneolivas777 <re.olivas@duocuc.cl>
2025-06-24 09:56:25 -0400

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,33 @@
+def menu():
+ reservas= 20
+ secreto= "EstoyEnListaDeReservas"
+ 
+ while True:
+     print("TOTEM AUTOATENCION RESERVA STRIKE")
+     print("1.Reservar zapatillas")
+     print("2.Buscar zapatillas reservadas")
+     print("3.Ver stock de reservas")
+     print("4.Salir.")
+     opcion=input("Ingrese una opcion (1-4): ")
+     if opcion=="1" and secreto =="EstoyEnListaDeReservas":
+           secreto=input("ingrese palabra secreta: ") 
+     elif opcion== "2":
+          pass
+     elif opcion =="3":
+          pass
+     elif opcion== "4":
+         break
+         assert ValueError 
+     print()
+     
+     
+print("--Reservar Zapatillas--")
+print("nombre comprador:")
+print("Reserva realizada exitosamente para :")
+
+menu()
+     
+        
+
+    
+      
